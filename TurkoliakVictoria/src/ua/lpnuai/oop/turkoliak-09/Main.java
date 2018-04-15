cd80660e3e2be996b9e0cf94ca8aab3e7c7e3b5a
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-04-15 22:22:16 +0300

    Можна маніпулювати

--- TurkoliakVictoria/src/ua/lpnuai/oop/turkoliak-09/Main.java
+++ TurkoliakVictoria/src/ua/lpnuai/oop/turkoliak-09/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         String[] array = new String[3];
-        array[0] = "Test";
-        array[1] = "Java .";
-        array[2] = "look";
+        array[0] = "Tested";
+        array[1] = "Javha .";
+        array[2] = "loojk";
         Container container = new Container(array);
         for (String item : container) {
             System.out.println(item);
@@ -34,7 +34,38 @@ public class Main {
             System.out.println(curr);
         }
 
+        System.out.println("enter 1 string to manip");
+        String str=scanner.nextLine();
+        System.out.println("1-add string,2-find,3-sort,4-delete all strings,5-display array,6-exit");
+        System.out.println("enter what you want to do");
+        int k=scanner.nextInt();
+            if(k==1){
+                container.add(str);
+                for (String item : container) {
+                    System.out.println(item);
+                }
+            }
+            else if(k==2){
+                System.out.println(container.find(str));
+            }
+            else if(k==3) {
+                container.sort();
+                for (int i = 0; i < array.length; i++)
+                    System.out.println(container.sort()[i]);
+            }
+            else if(k==4){
+                container.deleteAll();
+            }
+            else if(k==5) {
+                for (int i =0;i<array.length;i++)
+                System.out.println(array[i]);
+            }
+            else if(k==6){
+                System.out.println("Goodbye");
+                return;
+            }
+        }
 
     }
-    }
+
 
